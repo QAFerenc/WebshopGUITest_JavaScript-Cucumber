66ca1bb5be526c6994a7fc7aa4e6e7bc334a15ec
QAFerenc <87692147+QAFerenc@users.noreply.github.com>
2022-03-20 18:53:41 +0100

    Update product_page_operations.js

--- features/step-definitions/product_page_operations.js
+++ features/step-definitions/product_page_operations.js
@@ -3,10 +3,6 @@ const MainPage = require('../pages/main.page');
 const ProductPage = require('../pages/product.page');
 const CartPage = require('../pages/cart.page');
 
-
-
-
-
 When('Customer clicks Button Add to Cart',  async() => {
      ProductPage.pressButton("Add to cart")
 
