357b10d854c7b0322c90fced24d8d5061ba94939
QAFerenc <87692147+QAFerenc@users.noreply.github.com>
2022-03-20 18:53:09 +0100

    Update main_page_operations.js

--- features/step-definitions/main_page_operations.js
+++ features/step-definitions/main_page_operations.js
@@ -9,11 +9,7 @@ Given('A webpage as {string}', async(page) => {
   MainPage.open(page);
   browser.fullscreenWindow()
 })
-/*
-When('Customer clicks Button as {string}',  async(button) => {
-    await BasePage.pressButton(button)
-})
-*/
+
 When('Customer selects category as {string}',  async(category)  =>  {
     //  browser.fullscreenWindow()
     //  browser.pause(5000)
@@ -24,33 +20,15 @@ When('Customer picks laptop as {string}', async(laptop)  =>  {
      await MainPage.pressButton(laptop)
 })
 
-/*When('Customer clicks Button Add to Cart',  async() => {
-     ProductPage.addToCart("Add to cart")
-
-})
-
-When('Customer clicks Button Accept Alert',  () => {
-    browser.acceptAlert();
-})
-*/
 When('Customer clicks Home',  async() => {
     const elem = await $("//*[@id='navbarExample']/ul/li[1]/a");
     await elem.click();
 })
 
 When('Customer clicks Cart',  async() => {
-//    const elem= await $("//*[@id='cartur']")
-//    await elem.click();
     ProductPage.pressButton("Add to cart")
 })
 
-
-
-/*When('Customer clicks button Place Order',  async() => {
-    const elem = await $("//button[contains(text(),'Place Order')]")
-    await elem.waitForDisplayed();
-    await elem.click();
-})*/
 
 
 
