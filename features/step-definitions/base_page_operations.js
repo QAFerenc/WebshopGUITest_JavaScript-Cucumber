9b0ade672df80a2ab6c737b64cb3095f6f0175ac
QAFerenc <87692147+QAFerenc@users.noreply.github.com>
2022-03-20 18:50:23 +0100

    Update base_page_operations.js

--- features/step-definitions/base_page_operations.js
+++ features/step-definitions/base_page_operations.js
@@ -1,7 +1,4 @@
 const { Given, When, Then } = require('@cucumber/cucumber')
-//const MainPage = require('../pages/main.page');
-//const ProductPage = require('../pages/product.page');
-//const CartPage = require('../pages/cart.page');
 const BasePage = require('../pages/base.page');
 
 
@@ -9,17 +6,15 @@ When('Customer clicks button as {string}',  async(button) =>  {
     const elem = await $("//*[contains(text(),'"+button+"')]")
     await elem.waitForDisplayed();
     await elem.click();
-    //await MainPage.pressButton(button)
 })
 
 When('Customer accepts Alert', async() => {
-     await browser.pause(6000);
+    await browser.pause(6000);
     await browser.acceptAlert();
     await browser.pause(3000);
    
 
 })
-
 
 When('Customer clicks Button Add to Cart',  async() => {
      ProductPage.pressButton("Add to cart")
