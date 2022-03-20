4f15cbba522ce76fb1d93dba35baa073c8bf292a
QAFerenc <87692147+QAFerenc@users.noreply.github.com>
2022-03-20 17:45:36 +0100

    Add files via upload
    Initial commit

---
+++ features/pages/cart.page.js
@@ -0,0 +1,19 @@
+const BasePage = require('./base.page')
+
+class CartPage extends BasePage {
+
+
+    get laptop_price_locator () { return $('//*[@id="tbodyid"]/tr[1]/td[3]') }
+    get delete_second_element_locator () { return $('//*[@id="tbodyid"]/tr[2]/td[4]/a') }
+    get amount_locator()    { return $('/html/body/div[9]/p/text()[2]') }
+
+    /*placeOrder(button) {
+
+        super.pressButton(button)
+
+    }
+*/
+
+}
+
+module.exports = new CartPage();
---
+++ features/step-definitions/cart_page_operations.js
@@ -0,0 +1,94 @@
+const { Given, When, Then } = require('@cucumber/cucumber')
+const MainPage = require('../pages/main.page');
+const ProductPage = require('../pages/product.page');
+const CartPage = require('../pages/cart.page');
+
+
+When('Customer deletes last element',  async() => {
+    await browser.pause(2000);
+    const elem = await $("//*[@id='tbodyid']/tr[2]/td[4]/a");
+    await elem.waitForEnabled();
+    await elem.click();
+    await browser.pause(2000);
+})
+
+
+
+When('Customer clicks Button Place Order',  async() => {
+    CartPage.pressButton("Place Order")
+})
+
+
+When('Customer fills in field Name as {string}',  async(name) => {
+await browser.pause(500);
+    const elem = await $("//*[@id='name']");
+    await elem.waitForEnabled()
+    await elem.click();
+    await elem.setValue(name)
+})
+
+When('Customer fills in field Country as {string}',  async(country) => {
+await browser.pause(500);
+    const elem =  await $("//*[@id='country']");
+await elem.waitForEnabled()
+    await elem.click();
+   await elem.setValue(country)
+
+})
+
+When('Customer fills in field City as {string}',  async(city) => {
+    const elem = await $("//*[@id='city']");
+await elem.waitForEnabled()
+    await elem.click();
+    await elem.setValue(city)
+
+})
+
+
+When('Customer fills in field Card as {string}',  async(card) => {
+    const elem = await $("//*[@id='card']");
+await elem.waitForEnabled()
+    await elem.click();
+    await elem.setValue(card)
+
+})
+
+
+When('Customer fills in field Month as {string}',  async(month) => {
+    const elem = await $("//*[@id='month']");
+await elem.waitForEnabled()
+    await elem.click();
+    await elem.setValue(month)
+
+})
+
+When('Customer fills in field Year as {string}',  async(year) => {
+    const elem = await $("//*[@id='year']");
+await elem.waitForEnabled()
+    await elem.click();
+    await  elem.setValue(year)
+
+})
+
+
+
+When('Customer clicks Purchase Button',  async() => {
+    const elem = await $("//*[@id='orderModal']/div/div/div[3]/button[2]");
+await elem.waitForEnabled()
+    await elem.click();
+})
+
+
+
+Then('Customer clicks Close button',  async() => {
+    const elem = await $("//*[@id=\"orderModal\"]/div/div/div[3]/button[1]");
+await elem.waitForEnabled()
+    await elem.click();
+})
+
+
+
+Then('Match price as {string} with purchased amount', async(price) => {
+
+
+})
