c038ba90b45b72b4f0bb1c955ecf77b8e3da25a9
QAFerenc <87692147+QAFerenc@users.noreply.github.com>
2022-03-21 07:38:28 +0100

    Update cart.page.js

--- features/pages/cart.page.js
+++ features/pages/cart.page.js
@@ -6,6 +6,7 @@ class CartPage extends BasePage {
     get laptop_price_locator () { return $('//*[@id="tbodyid"]/tr[1]/td[3]') }
     get delete_second_element_locator () { return $('//*[@id="tbodyid"]/tr[2]/td[4]/a') }
     get amount_locator()    { return $('/html/body/div[9]/p/text()[2]') }
+    get closeButton()  {return    "//*[@id=\"orderModal\"]/div/div/div[3]/button[1]"}
 
 }
 
