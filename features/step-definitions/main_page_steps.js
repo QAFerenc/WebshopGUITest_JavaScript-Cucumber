c82830863872a07efe160aef3a9c9b638d09a7c0
QAFerenc <87692147+QAFerenc@users.noreply.github.com>
2022-03-21 07:16:01 +0100

    Update main_page_steps.js

--- features/step-definitions/main_page_steps.js
+++ features/step-definitions/main_page_steps.js
@@ -11,10 +11,8 @@ Given('A webpage as {string}', async(page) => {
 })
 
 When('Customer selects category as {string}',  async(category)  =>  {
-    await MainPage.pressButton(category)
+      await MainPage.pressButton(category)
 })
-
-// the whole list of laptops are input parameters, and one of them is selected according to the value of index
 
 When('Customer picks index as {string} laptops as {string}', async(index,laptops)  =>  {
      ind = parseInt(index) - 1
